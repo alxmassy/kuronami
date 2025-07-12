30788e3e18a93253483eba4cbd96504a451fc6db
alxmassy <alexmassy2005@gmail.com>
2025-07-12 15:44:20 +0530

    Updated for API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://https://876e77a8dfa7.ngrok-free.app/:3001/api';
+const API_URL = 'http://localhost:3001/api' || 'http://https://876e77a8dfa7.ngrok-free.app/:3001/api';
 
 const api = axios.create({
   baseURL: API_URL,
